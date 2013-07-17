d86a7bb3fcaa1584d190baf8a452c72c7bf4ec53
R Dragone <robert.dragone@gmail.com>
2013-07-16 20:42:10 -0400

    Update to NSB

--- NSB/NSB.cc.lua
+++ NSB/NSB.cc.lua
@@ -5,6 +5,8 @@
 -- Namespace
 local nsb = {}
 
+local nsb.startup = ""
+
 -- HTTP API Check
 if (not http) then
   print("!! Enable HTTP before running this tool.")
@@ -16,7 +18,7 @@ end
 -- GitHub Functionality
 function nsb.gitGet (gUser, gProject, gFile)
   local gURL = "https://raw.github.com/" .. gUser .. "/" ..
-                gProject .. "/" .. gFile
+                gProject .. "/master/" .. gFile
   local gResponse = http.get(gURL)
   if (not gResponse) then
     return nil
@@ -26,14 +28,25 @@ end
 
 -- Retrieve Tab Completion
 print("Cloning ReadOverride for Tab Completion...")
-local nsb.fileRaw = nsb.gitGet ("oneirosFade", "CC", "master/External/ReadOverride.cc.lua")
+local nsb.fileRaw = nsb.gitGet("oneirosFade", "CC", "master/External/ReadOverride.cc.lua")
 print("Installing ReadOverride...")
 fs.makeDir("/sys")
 local nsb.file = fs.open("/sys/ReadOverride.cc.lua", "w")
 nsb.file.write(nsb.fileRaw)
 nsb.file.close()
-print("You must add a line to your startup which reads:")
-print("  shell.run(\"/sys/ReadOverride.cc.lua\")")
-print("and reboot for this to take effect.")
+if (fs.exists("/startup")) then
+  print("You must add a line to your startup which reads:")
+  print("  shell.run(\"/sys/ReadOverride.cc.lua\")")
+  print("and reboot for this to take effect.")
+else
+  nsb.startup = nsb.startup .. 
+    "shell.run(\"/sys/ReadOverride.cc.lua\")\n"
+end
 
-
+-- If no startup exists, write one
+if (not fs.exists("/startup")) then
+  print("Creating startup file...")
+  local hStartup = fs.open("/startup", "w")
+  hStartup.write(nsb.startup)
+  hStartup.close()
+end
